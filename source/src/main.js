a728f51a46b8f58793fc6bee2ef92b8d0bb4d27a
ehsan shariati <ehsan6sha@gmail.com>
2021-07-10 14:56:04 -0400

    Added message

--- source/src/main.js
+++ source/src/main.js
@@ -1,16 +1,16 @@
 import { Actor, HttpAgent, Principal } from "@dfinity/agent";
 import { DelegationIdentity } from "@dfinity/identity";
 import { AuthClient } from "@dfinity/auth-client";
-
+import {MDCSnackbar} from '@material/snackbar';
 
 
 const signInBtn = document.getElementById("signinBtn");
 const signOutBtn = document.getElementById("signoutBtn");
-const msg = document.getElementById("msg");
-
+const snackbar = new MDCSnackbar(document.querySelector('.mdc-snackbar'));
+const redirectBtn = document.getElementById("redirectBtn");
 
 let authClient;
-
+let appUri = '';
 
 const init = async () => {
   authClient = await AuthClient.create();
@@ -23,17 +23,22 @@ const init = async () => {
 		
       const publicKey = (identity.getDelegation().toJSON()).publicKey.toString();
 	  //redirect to app here
-	  const appUri = "exp://192.168.68.117:19000/--/Photos?principal="+principal+"&publicKey=";
-	  msg.innerHTML = "Redirecting to application now. Click <a href='"+appUri+"'>HERE</a> if not redirected automatically";
+	  appUri = "exp://192.168.68.117:19000/--/Photos?principal="+principal+"&publicKey=";
+	  snackbar.open();
+	  
 	  setTimeout(()=>{
 		window.location = appUri;
 	  },20);
-    }else{
-		msg.innerHTML = ""
-	}
+    }
   }
 
   updateView();
+  
+  redirectBtn.onclick = async () => {
+    setTimeout(()=>{
+		window.location = appUri;
+	},10);
+  };
 
   signInBtn.onclick = async () => {
     authClient.login({
