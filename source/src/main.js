eb6d9a211ab37ec22cde1bbb274f4092c17c92c2
ehsan shariati <ehsan6sha@gmail.com>
2021-07-25 22:11:25 -0400

    Sending delegation
    Sending delegation instead of just public key

--- source/src/main.js
+++ source/src/main.js
@@ -32,10 +32,10 @@ const init = async () => {
     const principal = identity.getPrincipal();
     if (identity instanceof DelegationIdentity) {
 	  signInBtn.disabled = true;
-      const publicKey = (identity.getDelegation().toJSON()).publicKey;
-	  const publicKey64 = new Buffer(publicKey).toString("base64");
+      const delegation = (identity.getDelegation().toJSON());
+	  const delegation64 = new Buffer(delegation).toString("base64");
 	  //redirect to app here
-	  appUri = "exp://192.168.68.117:19000/--/Photos?principal="+principal+"&publicKey64="+publicKey64;
+	  appUri = "exp://192.168.68.117:19000/--/Photos?principal="+principal+"&delegation64="+delegation64;
 	  
 	  snackbar.open();
 	  snackbar.timeoutMs = -1;
